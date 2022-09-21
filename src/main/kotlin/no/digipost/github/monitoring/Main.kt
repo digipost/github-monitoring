8617abb05b097144a2c32891043f9c00d928d9af
Eivind Bergstøl <eivind@bergstol.no>
2022-09-21 23:48:31 +0200

    Legg til metrikker for info og score for sårbarhet

--- src/main/kotlin/no/digipost/github/monitoring/Main.kt
+++ src/main/kotlin/no/digipost/github/monitoring/Main.kt
@@ -24,7 +24,7 @@ val LANGUAGES = setOf("JavaScript", "Java", "TypeScript", "C#", "Kotlin")
 
 suspend fun main(): Unit = coroutineScope {
     val env = System.getenv("env")
-    val token = if(env == "local") System.getenv("token") else withContext(Dispatchers.IO) {
+    val token = if (env == "local") System.getenv("token") else withContext(Dispatchers.IO) {
         Files.readString(Path.of("/secrets/githubtoken.txt")).trim()
     }
 
@@ -33,7 +33,11 @@ suspend fun main(): Unit = coroutineScope {
 
     ApplicationInfoMetrics().bindTo(prometheusMeterRegistry)
 
-    val multiGauge = MultiGauge.builder("repository_vulnerability_count")
+    val multiGaugeRepoVulnCount = MultiGauge.builder("repository_vulnerability_count")
+        .tags("owner", "digipost")
+        .register(prometheusMeterRegistry)
+
+    val multiGaugeInfoScore = MultiGauge.builder("vulnerability_info_score")
         .tags("owner", "digipost")
         .register(prometheusMeterRegistry)
 
@@ -45,7 +49,7 @@ suspend fun main(): Unit = coroutineScope {
     launch {
         while (isActive) {
             val timeMillis = measureTimeMillis {
-                publish(apolloClient, multiGauge)
+                publish(apolloClient, multiGaugeRepoVulnCount, multiGaugeInfoScore)
             }
             logger.info("Henting av repos med sårbarheter tok ${timeMillis}ms")
             delay(1000 * 60 * 5)
@@ -57,7 +61,7 @@ suspend fun main(): Unit = coroutineScope {
         .startMetricsServer(prometheusMeterRegistry, 9610)
 }
 
-suspend fun publish(apolloClient: ApolloClient, register: MultiGauge): Unit = coroutineScope {
+suspend fun publish(apolloClient: ApolloClient, registerRepos: MultiGauge, registerVulnerabilites: MultiGauge): Unit = coroutineScope {
 
     val channel = Channel<Repos>()
     launch {
@@ -72,9 +76,25 @@ suspend fun publish(apolloClient: ApolloClient, register: MultiGauge): Unit = co
             logger.info("Antall repos: ${all.size}")
             logger.info("Antall med sårbarheter: ${onlyVulnerable.size}")
             logger.info("Antall sårbarheter å rette: ${onlyVulnerable.flatMap { it.vulnerabilities }.count()}")
-        }.all.map { repo ->
-            MultiGauge.Row.of(Tags.of("name", repo.name, "language", repo.language), repo.vulnerabilities.size)
-        }.let { register.register(it) }
+
+            all.map { repo ->
+                MultiGauge.Row.of(Tags.of("name", repo.name, "language", repo.language), repo.vulnerabilities.size)
+            }.let { registerRepos.register(it) }
+
+            onlyVulnerable.map { repo ->
+                repo.vulnerabilities.map { vuln ->
+                    MultiGauge.Row.of(
+                        Tags.of(
+                            "name", repo.name,
+                            "language", repo.language,
+                            "CVE", vuln?.CVE ?: "",
+                            "packagename", vuln?.packageName ?: "UNKNOWN",
+                            "severity", vuln?.severity ?: "UNKNOWN",
+                        ), vuln?.score ?: 0.0
+                    )
+                }
+            }.flatMap { it.toList() }.let { registerVulnerabilites.register(it) }
+        }
     }
 
 }
